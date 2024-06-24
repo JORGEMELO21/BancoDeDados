5191bbdec399739b9273fe89efedf103a6f7669f
JORGEMELO21 <142284641+JORGEMELO21@users.noreply.github.com>
2024-06-24 18:31:36 -0300

    Add files via upload

---
+++ BANCO DE DADOS/w3-schools/banco-de-dados-carros.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE carros(
+-- 	marca VARCHAR (255),
+-- 	modelo VARCHAR (255),
+-- 	ano INT,
+-- 	cor VARCHAR (10)
+-- );
+
+INSERT INTO carros(marca, modelo, ano)
+VALUES ('Ford', 'Mustang', 1964);
+
+SELECT * FROM carros;
+
+INSERT INTO carros(marca, modelo, ano)
+VALUES ('Ford', 'Ka', 1994);
+
+INSERT INTO carros(marca, modelo, ano)
+VALUES ('Fiat', 'Uno', 1999);
+
+select * from carros;
